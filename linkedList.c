9d46cfebac6e37efe23aba3231eb7e370f0cb511
claytoPro <claytonprovan@gmail.com>
2018-04-28 11:32:35 -0400

    first entry

---
+++ linkedList.c
@@ -0,0 +1,136 @@
+/*
+By:clayton provan
+create linked list with pointers. includes functions to alphabetise data.
+check if linked list contains data. reverse linked list. 
+*/
+
+#include "linkedList.h"
+
+int main(){
+    struct listNode **list = malloc(sizeof(Node)); 
+
+    push(list,newNode("apple"));
+    push(list,newNode("butter"));
+    push(list,newNode("eat"));
+    push(list,newNode("cock"));
+    push(list,newNode("dick"));
+    
+    printLinkedList(*list);
+    printf("\n");
+    reverseOrder(list);
+    printLinkedList(*list);
+
+    alphaSort(list, 5);
+
+
+    deleteList(list);
+    
+
+    return 0;
+}
+
+/* Mandatory: Frees all allocated memory associated with the list pointers iteratively */
+void deleteList(Node **list) {
+    Node *temp = NULL; 
+    while( (*list) != NULL){
+        temp = *list;
+        deleteNode(list);
+        *list = temp->next;
+    }
+    free(list);
+}
+
+/* Mandatory: Frees all allocated memory associated with a single node */
+void deleteNode(Node **toDelete){
+    free((*toDelete));
+}
+
+/* Mandatory: Allocates memory for a new string and returns a pointer to the memory. */
+Node *newNode(char *string) {
+        struct listNode *new = malloc(sizeof(Node));
+        new->data = string;
+        return new;
+}
+
+/* Mandatory: Removes a node from the front of a list */
+Node *pop(Node **list) {
+        struct listNode *temp = *list; 
+        *list = temp->next;
+    return temp;
+}
+
+/* Mandatory: Adds a node to the front of a list */
+void push(Node **list, Node *toAdd) {
+        toAdd->next = *list; 
+        *list = toAdd;
+}
+
+/* Mandatory: Return a list of pointers in order */
+void reverseOrder(Node **list) {
+    /* Hint: your solution should not need to allocate memory */
+    /* remember push and pop... */
+    
+    Node **reverseList = NULL;
+    Node *temp =NULL;
+    reverseList = malloc(sizeof(Node));
+    if(list == NULL){
+        return;
+    }
+ 
+    while(*list){
+        temp = pop(list);
+        push(reverseList,temp);
+
+    }
+    /*set pointer equal to reversed list pointer, list is null currently*/
+    *list = *reverseList;
+    free(reverseList);
+}
+
+/* Optional: Prints an entire linked list. Nodes are printed from first to last. */
+void printLinkedList(Node *list) {
+
+ while(list != NULL){
+        /*TODO %p expects pointer*/
+        printf("holds %s\n",list->data);
+        list = list->next;
+    }
+}
+/*sort words alpha betically by copying them to a list of words, sorting them.
+then reassigning the values in the linked list*/
+void alphaSort(Node **list,int numStrings){
+    char **words = malloc(sizeof(char*) * numStrings);
+    int i=0;
+    if(list == NULL){
+        return;
+    }
+
+    /*takes in all words stored in the linked list*/
+    while(*list){
+        words[i] = (*list)->data;
+        (*list) = (*list)->next;
+        i++;
+        
+    }
+
+    qsort(words,numStrings,sizeof(char*),cmp);
+
+    for(i=0; i<numStrings;i++){
+        printf("%s \n",words[i]);
+        
+    }
+   free(words);
+    
+
+}
+
+/*moves pointers around to sort them alphabetically*/
+void sortLinks(Node **list){
+
+
+
+}
+
+int cmp(const void *a, const void *b){
+    return(strcmp(*((char**)a), *((char **)b)));
+}
---
+++ linkedList.h
@@ -0,0 +1,62 @@
+
+
+/*****
+* Standard Libraries
+*****/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/*****
+* Self-defined Data Structures
+*****/
+
+/* The listNode data type for storing entries in a linked list */
+
+typedef struct listNode Node;
+struct listNode {
+    char *data;
+    Node *next;
+};
+
+
+/*****
+* Function Prototypes for linkedList.c
+*****/
+
+
+/***** Mandatory Functions *****/
+
+
+/* Mandatory: Frees all allocated memory associated with the list pointers iteratively */
+void deleteList(Node **list);
+
+/* Mandatory: Frees all allocated memory associated with a single node */
+void deleteNode(Node **toDelete);
+
+/* Mandatory: Allocates memory for a new string and returns a pointer to the memory */
+Node *newNode(char *string);
+
+/* Mandatory: Removes a node from the front of a list and returns a pointer to said node */
+Node *pop(Node **list);
+
+/* Mandatory: Adds a node to the front of a list */
+void push(Node **list, Node *toAdd);
+
+/* Mandatory: Return a list of pointers in order */
+void reverseOrder(Node **list);
+
+/* Optional: Prints an entire linked list. Nodes are printed from first to last. */
+void printLinkedList(Node *linkedList);
+
+/*Alphabetise data. but does not move pointers, just reassigns values for data*/
+void alphaSort(Node **list,int numStrings);
+
+/*sorts links */
+void sortLinks(Node **list);
+
+/*qsort compare function*/
+int cmp(const void *a, const void *b);
+
